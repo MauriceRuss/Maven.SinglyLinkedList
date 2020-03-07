c2c8896bf215887e24c77c5b417637d180a64260
Mo Russ <mauriceruss302@gmail.com>
2020-03-07 12:19:43 -0500

    method stubs made

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -7,6 +7,36 @@ public class SinglyLinkedList {
     public SinglyLinkedList() {
     }
 
+    public void addElement(int element){}
+
+    public void removeElementByIndex(int index){}
+
+    public boolean containsElement(int element){
+        return false;
+    }
+
+    public Integer findElementByIndex (int index){
+        return null;
+    }
+
+    public Integer getSizeOfList(){
+        return null;
+    }
+
+    public Integer getElementByIndex(){
+        return null;
+    }
+
+    public SinglyLinkedList copysSameValues(){
+        return null;
+    }
+
+    public void sortList(){}
+
+
+
+
+
     class Node {
         int element;
         Node next;
