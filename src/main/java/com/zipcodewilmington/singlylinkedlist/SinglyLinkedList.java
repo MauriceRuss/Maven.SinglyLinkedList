3a48441ea44cf34fb6b02a85772b46b891912185
Mo Russ <mauriceruss302@gmail.com>
2020-03-07 12:00:00 -0500

    Node class now has fields and constructor

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,7 +4,16 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    public SinglyLinkedList() {
+    }
+
     class Node {
-        
+        int element;
+        Node next;
+
+        public Node (int element, Node next){
+            this.element = element;
+            this.next = next;
+        }
     }
 }
